ff1f5644a6fdf0eb1f4a19b43561dcfa162534b0
Peemin <peemin.chen@gmail.com>
2014-04-09 09:33:09 -0600

    added WordObject.cs

---
+++ KinectWords/KinectWords/WordObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinectWords
+{
+    class WordObject
+    {
+        string word;
+        string text1;
+        string text2;
+        string text3;
+
+        public WordObject(string word, string text1, string text2, string text3)
+        {
+            this.word = word;
+            this.text1 = text1;
+            this.text2 = text2;
+            this.text3 = text3;
+
+        }
+
+        public string getText1()
+        {
+            return text1;
+        }
+        public string getText2()
+        {
+            return text2;
+        }
+        public string getText3()
+        {
+            return text3;
+        }
+
+        public string getWord()
+        {
+            return word;
+        }
+    }
+}
